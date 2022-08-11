ecb83b2faabe8514bd34f14f85ee6c1f7db6219c
FoRA <48875566+fora22@users.noreply.github.com>
2022-08-12 00:01:30 +0900

    멘토링 #1 완료

--- study/basic/짝수와 홀수 #1/soulution.py
+++ study/basic/짝수와 홀수 #1/soulution.py
@@ -1,5 +1,16 @@
 def solution(num):
+    '''
+    목적: 짝수, 홀수 구분해주는 함수
+    '''
     answer = ''
+
+    # %는 파이썬에서 나머지 연산을 수행함
+    if num % 2==0:
+        # 숫자에서 2로 나눈 나머지가 0이면 짝수
+        answer = 'Even'
+    else:
+        # 아닐 경우 홀수
+        answer ='Odd'
     return answer
 
 print(solution(3))
--- study/basic/최소직사각형 #2/soulution.py
+++ study/basic/최소직사각형 #2/soulution.py
@@ -1,3 +1,34 @@
 def solution(sizes):
+    '''
+    목적: 최소 직사각형의 넓이를 계산해주는 함수
+    '''
     answer = 0
+    # 리스트(list) 안에 있는 아이템(item) 반복
+    width = []
+    height = []
+    for _, item in enumerate(sizes):
+        # item에서 0번째는 w, 1번째는 h
+        # width, height : 너비, 높이
+        w = item[0] # width
+        h = item[1] # height
+        
+        # w랑 h비교 -> h 가 클 경우, w랑 h랑 교체
+        if w < h:
+            temp = h
+            h = w
+            w = temp
+        
+        width.append(w)
+        height.append(h)
+
+    # 마지막으로 w 중에 최댓값, h 중에 최댓값 구하기
+    max_w = max(width)
+    max_h = max(height)
+
+    # return w 최댓값 * h 최댓값 
+    answer = max_w * max_h
     return answer
+
+print(solution([[60,50],[30,70],[60,30],[80,40]]))
+print(solution([[10,7],[12,3],[8,15],[14,7],[5,15]]))
+print(solution([[14,4],[19,6],[6,16],[18,7],[7,11]]))
